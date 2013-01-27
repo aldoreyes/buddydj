a387896de7f615facb64bd87ab792a8d1a9346d2
Matias Penela <map@matiaz.com>
2013-01-26 23:26:25 -0500

    bootstrap

--- web/index.php
+++ web/index.php
@@ -1,1 +1,58 @@
-fdj web app
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Bootstrap 101 Template</title>
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="css/main.css" rel="stylesheet" media="screen">
+</head>
+<body>
+<div id="main">
+</div>
+
+<div class="panel" id="home">
+	<div class="container-narrow">
+
+		<div class="masthead">
+			<h3 class="muted">Facebook DJ</h3>
+		</div>
+
+		<hr>
+
+		<div class="jumbotron">
+	        <h1>What are your friends jamming to?</h1>
+	        <p class="lead">Let your friends be the DJ. Facebook DJ will play what your friends are listening to in real-time.</p>
+	        <a class="btn btn-primary btn-large" href="#">Sign in with Facebook</a>
+	      </div>
+
+	      <hr>
+	      <div class="row-fluid marketing">
+	        <div class="span6">
+	          <h4>Subheading</h4>
+	          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+	        </div>
+
+	        <div class="span6">
+	          <h4>Subheading</h4>
+	          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+	        </div>
+				</div>
+	</div>
+</div>
+<div class="panel" id="player">
+	this is the player div!
+</div>
+
+<div id="debugpanel">
+	Debug Panel:
+	<br>
+	<a href="#" onClick="javascript:onUserLogin();">onUserLogin()</a>
+	<br>
+	<a href="#" onClick="javascript:onUserLogout();">onUserLogout()</a>
+<div> 
+
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
