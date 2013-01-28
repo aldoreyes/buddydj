7739497ba3fcc34da0d252654b2753a87757ea1b
Aldo Reyes <aldo.reyes18@gmail.com>
2013-01-27 22:57:09 -0600

    rdio artists

--- web/js/app.js
+++ web/js/app.js
@@ -145,6 +145,27 @@ var FDJ = {
 			this.Models.Song = Backbone.Model.extend({
 				initialize:function(){
 					this.set('publish_time_mili', Date.parse(this.get('publish_time')));
+
+					//create artist homogenous property
+					switch(this.get('application').name){
+					case "spotify":
+						this.set('artist', this.get('musician').title);
+						break;
+					case "Rdio":
+						var groups = this.constructor.rdioParse(this.get('data').song.url);
+						this.set('artist', groups[1].replace(/_/g, ' '));
+						this.set('album', groups[2].replace(/_/g, ' '));
+						break;
+					default:
+						this.set('artist','');
+						break;
+					}
+				}
+			},
+			//STATIC
+			{
+				rdioParse:function(url){
+					return /^.*artist\/(.+)\/album\/(.+)\/track\/.*/g.exec(url)
 				}
 			});
 
