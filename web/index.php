338a1a6c73bba95c95daff43c569d777774426f3
Matias Penela <map@matiaz.com>
2013-02-17 01:48:48 -0500

    mongo test

--- web/index.php
+++ web/index.php
@@ -8,6 +8,54 @@
 		<link href="css/main.css" rel="stylesheet" media="screen">
 	</head>
 	<body>
+		<?php
+
+    // connect to MongoHQ assuming your MONGOHQ_URL environment
+    // variable contains the connection string
+    $connection_url = getenv("mongodb://fdjdb:49ikk29s8@linus.mongohq.com:10048/app11333631");
+ 
+    // create the mongo connection object
+    $m = new Mongo($connection_url);
+ 
+    // extract the DB name from the connection path
+    $url = parse_url($connection_url);
+    $db_name = preg_replace('/\/(.*)/', '$1', $url['path']);
+ 
+    // use the database we connected to
+    $db = $m->selectDB($db_name);
+ 
+    echo "<h2>Collections</h2>";
+    echo "<ul>";
+ 
+    // print out list of collections
+    $cursor = $db->listCollections();
+    $collection_name = "";
+    foreach( $cursor as $doc ) {
+      echo "<li>" .  $doc->getName() . "</li>";
+      $collection_name = $doc->getName();
+    }
+    echo "</ul>";
+ 
+    // print out last collection
+    if ( $collection_name != "" ) {
+      $collection = $db->selectCollection($collection_name);
+      echo "<h2>Documents in ${collection_name}</h2>";
+ 
+      // only print out the first 5 docs
+      $cursor = $collection->find();
+      $cursor->limit(5);
+      echo $cursor->count() . ' document(s) found. <br/>';
+      foreach( $cursor as $doc ) {
+        echo "<pre>";
+        var_dump($doc);
+        echo "</pre>";
+      }
+    }
+ 
+    // disconnect from server
+    $m->close();
+]
+?>
 		<!--
 		***************************************
 		***** Main Container: Where the magic happens
