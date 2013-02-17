d107bd1b1d61ba79206f2b027fa54fab88e53bc6
Matias Penela <map@matiaz.com>
2013-02-16 20:21:26 -0500

    iphone viewport

--- web/index.php
+++ web/index.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<title>BuddyDJ</title>
+		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
 		<!-- Bootstrap -->
 		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 		<link href="css/main.css" rel="stylesheet" media="screen">
