b64c5f0789c743f96e9d4e82f3deb2eff43608e6
Matias Penela <map@matiaz.com>
2013-02-20 01:56:47 -0500

    final setting on node heroku

--- web/node.php
+++ web/node.php
@@ -1,7 +1,7 @@
-<script src="http://fdjnode.herokuapp.com//socket.io/socket.io.js"></script>
+<script src="http://fdjnode.herokuapp.com/socket.io/socket.io.js"></script>
 <script>
   var socket = io.connect('http://fdjnode.herokuapp.com');
-  socket.emit('getYtSong', { songId: '9988',songName:'fromHeroku' });
+  socket.emit('getYtSong', { songId: '44',songName:'libertadores' });
   
   socket.on('onGetYtSong', function (data) {
     console.log(data);
