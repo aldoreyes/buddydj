d27120174f1c989e6a50ad57a9c38db60c2ed7ec
Matias Penela <map@matiaz.com>
2013-02-20 01:41:39 -0500

    node heroku socket.io ver 0.9.13

--- web/node.php
+++ web/node.php
@@ -1,7 +1,7 @@
-<script src="http://127.0.0.1:8080/socket.io/socket.io.js"></script>
+<script src="http://fdjnode.herokuapp.com//socket.io/socket.io.js"></script>
 <script>
-  var socket = io.connect('http://127.0.0.1:8080');
-  socket.emit('getYtSong', { songId: '44',songName:'libertadores' });
+  var socket = io.connect('http://fdjnode.herokuapp.com');
+  socket.emit('getYtSong', { songId: '9988',songName:'fromHeroku' });
   
   socket.on('onGetYtSong', function (data) {
     console.log(data);
