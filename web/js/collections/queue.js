46c4de3353a26dd153ab2010beeae00bdb5ffd00
Aldo Reyes <aldo.reyes18@gmail.com>
2013-02-24 23:57:50 -0600

    skip not found

--- web/js/collections/queue.js
+++ web/js/collections/queue.js
@@ -1,5 +1,5 @@
 FDJ.Collections.Queue = Backbone.Collection.extend({
     model:FDJ.Models.Song,
-    //url:"http://fdjnode.herokuapp.com/song/"
-    url:"http://localhost:5000/song/"
+    url:"http://fdjnode.herokuapp.com/song/"
+    //url:"http://localhost:5000/song/"
 });
