fba4383c1d59b9bfba5a6c30fe68875c53f41cf5
Matias Penela <map@matiaz.com>
2013-02-25 00:38:37 -0500

    fix returning video id on node side

--- web/js/collections/queue.js
+++ web/js/collections/queue.js
@@ -1,4 +1,5 @@
 FDJ.Collections.Queue = Backbone.Collection.extend({
-	model:FDJ.Models.Song,
-	url:"http://fdjnode.herokuapp.com/song/"
+    model:FDJ.Models.Song,
+    //url:"http://fdjnode.herokuapp.com/song/"
+    url:"http://localhost:5000/song/"
 });
